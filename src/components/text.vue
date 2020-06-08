ea1fabe0a6576426a0ce80479ea12e4aa3ac6902
dandan <1336124717@qq.com>
2020-06-08 11:16:44 +0800

    动态引入组件并进行组件间传值测试

---
+++ src/components/text.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{parentData}}</div>
+</template>
+<script>
+export default {
+  name: 'textCom',
+  props: {
+    parentData: {
+      type: String
+    }
+  }
+
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <button @click="changeCom">点击动态切换组件</button>
+    <component :is="nowComponent"
+               v-bind:parentData="parentData"></component>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import textCom from '@/components/text.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    textCom
+  },
+  data () {
+    return {
+      nowComponent: HelloWorld,
+      parentData: '来自父组件中的值'
+    }
+  },
+  mounted: {
+
+  },
+  created () {
+
+  },
+  methods: {
+    changeCom () {
+      this.nowComponent === HelloWorld ? this.nowComponent = textCom : this.nowComponent = HelloWorld
+    }
+  }
+}
+</script>
